97bf2f779a0d8c66b27c7c5ff09fc5871d757842
hathanhdung1995 <hathanhdung1995@gmail.com>
2021-02-22 15:55:26 +0700

    Hello

---
+++ ShopApplication/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShopApplication.Application.Interfaces;
+
+namespace ShopApplication.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Data Api
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new ObjectResult(model);
+        }
+
+
+        #endregion
+        }
+}
