d4e95727016ab4899f0a9d8de6e1bef5290b89c3
hathanhdung1995 <hathanhdung1995@gmail.com>
2021-02-25 08:46:54 +0700

    Tạo trang danh mục sản phẩm

--- ShopApplication/Controllers/ProductController.cs
+++ ShopApplication/Controllers/ProductController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Configuration;
 using ShopApplication.Application.Interfaces;
+using ShopApplication.Models.ProductViewModels;
 
 namespace ShopApplication.Controllers
 {
@@ -32,7 +34,41 @@ namespace ShopApplication.Controllers
         [Route("{alias}-c.{id}.html")]
         public IActionResult Catalog(int id, string keyword, int? pageSize, string sortBy, int page = 1)
         {
-            return View();
+            var catalog = new CatalogViewModel();
+            ViewData["BodyClass"] = "shop_grid_full_width_page";
+            if (pageSize == null)
+                pageSize = _configuration.GetValue<int>("PageSize");
+
+            catalog.PageSize = pageSize;
+            catalog.SortType = sortBy;
+            catalog.Data = _productService.GetAllPaging(id, string.Empty, page, pageSize.Value);
+            catalog.Category = _productCategoryService.GetById(id);
+
+            return View(catalog);
+        }
+        [Route("{alias}-p.{id}.html", Name = "ProductDetail")]
+        public IActionResult Details(int id)
+        {
+            ViewData["BodyClass"] = "product-page";
+            var model = new DetailViewModel();
+            model.Product = _productService.GetById(id);
+            model.Category = _productCategoryService.GetById(model.Product.CategoryId);
+            model.RelatedProducts = _productService.GetRelatedProducts(id, 9);
+            model.UpsellProducts = _productService.GetUpsellProducts(6);
+            model.ProductImages = _productService.GetImages(id);
+            model.Tags = _productService.GetProductTags(id);
+            model.Colors = _billService.GetColors().Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }).ToList();
+            model.Sizes = _billService.GetSizes().Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }).ToList();
+
+            return View(model);
         }
     }
 }
