eee027e139afe8a875a4aea67c0fa6ddd4234017
hathanhdung1995 <hathanhdung1995@gmail.com>
2021-02-25 19:11:09 +0700

    Cart Operation

--- ShopApplication/Controllers/ProductController.cs
+++ ShopApplication/Controllers/ProductController.cs
@@ -28,7 +28,8 @@ namespace ShopApplication.Controllers
         [Route("products.html")]
         public IActionResult Index()
         {
-            return View();
+            var categories = _productCategoryService.GetAll();
+            return View(categories);
         }
 
         [Route("{alias}-c.{id}.html")]
