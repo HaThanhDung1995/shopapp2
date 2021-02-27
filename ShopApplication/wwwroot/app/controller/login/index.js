a0a415a933d77e35e966b08c1a91431d075d294a
hathanhdung1995 <hathanhdung1995@gmail.com>
2021-02-27 08:55:21 +0700

    SEO Website

--- ShopApplication/wwwroot/app/controller/login/index.js
+++ ShopApplication/wwwroot/app/controller/login/index.js
@@ -1,20 +1,20 @@
 ﻿var loginController = function () {
-//    Phương thức tỉnh khi chạy lần đầu tiên
-    this.initialize = function() {
+    this.initialize = function () {
         registerEvents();
     }
+
     var registerEvents = function () {
         $('#frmLogin').validate({
             errorClass: 'red',
             ignore: [],
             lang: 'en',
             rules: {
-                userName: "required",
-                password: "required"
-            },
-            message: {
-                userName: "you have to enter",
-                password: "you have to enter"
+                userName: {
+                    required: true
+                },
+                password: {
+                    required: true
+                }
             }
         });
         $('#btnLogin').on('click', function (e) {
@@ -23,24 +23,23 @@
                 var user = $('#txtUserName').val();
                 var password = $('#txtPassword').val();
                 login(user, password);
-           }
+            }
 
         });
     }
+
     var login = function (user, pass) {
         $.ajax({
             type: 'POST',
             data: {
-                Username: user,
+                Email: user,
                 Password: pass
             },
             dateType: 'json',
             url: '/admin/login/authen',
             success: function (res) {
-                console.log(res);
                 if (res.Success) {
-                    
-                   window.location.href = "/Admin/Home/Index";
+                    window.location.href = "/Admin/Home/Index";
                 } else {
                     tedu.notify('Login failed', 'error');
                 }
